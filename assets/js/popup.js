57b2c27a25b374aa74fc9382fd949f4686bad42b
Lee-ji-soo <nbsp3@naver.com>
2020-08-28 12:55:37 +0900

    lang=kr로변경

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -20,45 +20,45 @@ searchFilterPopupCloseBtn.addEventListener('click',
 const popupIssueOL = document.getElementById('searchfilter_popup-issues');
 
 const popupIssues = [{
-        num: 1,
-        title: '악인전'
-    },
-    {
-        num: 2,
-        title: '바르셀로나 뮌헨'
-    },
-    {
-        num: 3,
-        title: '나지완'
-    },
-    {
-        num: 4,
-        title: '박희순 박예진'
-    },
-    {
-        num: 5,
-        title: '김원준'
-    },
-    {
-        num: 6,
-        title: 'sf8'
-    },
-    {
-        num: 7,
-        title: '박예진'
-    },
-    {
-        num: 8,
-        title: '김원준 나이'
-    },
-    {
-        num: 9,
-        title: '박희순'
-    },
-    {
-        num: 10,
-        title: '아쿠아맨'
-    },
+    num: 1,
+    title: '악인전'
+},
+{
+    num: 2,
+    title: '바르셀로나 뮌헨'
+},
+{
+    num: 3,
+    title: '나지완'
+},
+{
+    num: 4,
+    title: '박희순 박예진'
+},
+{
+    num: 5,
+    title: '김원준'
+},
+{
+    num: 6,
+    title: 'sf8'
+},
+{
+    num: 7,
+    title: '박예진'
+},
+{
+    num: 8,
+    title: '김원준 나이'
+},
+{
+    num: 9,
+    title: '박희순'
+},
+{
+    num: 10,
+    title: '아쿠아맨'
+},
 ]
 
 function printPopupIssues() {
@@ -116,7 +116,5 @@ function handleFilterRange(e) {
         }
     }
 }
-
-
 
 filterRangeOL.forEach(ol => ol.addEventListener('click', handleFilterRange))
